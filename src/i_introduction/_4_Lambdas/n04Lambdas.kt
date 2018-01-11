a8fa8c3c53ee898217749ec20ef37c855a742a39
Raj <the.raj.saxena@gmail.com>
2018-01-11 09:42:37 +0100

    Solutions 4 and 5

--- src/i_introduction/_4_Lambdas/n04Lambdas.kt
+++ src/i_introduction/_4_Lambdas/n04Lambdas.kt
@@ -1,8 +1,5 @@
 package i_introduction._4_Lambdas
 
-import util.TODO
-import util.doc4
-
 fun example() {
 
     val sum = { x: Int, y: Int -> x + y }
@@ -11,15 +8,6 @@ fun example() {
     sum(1, square(2)) == 5
 }
 
-fun todoTask4(collection: Collection<Int>): Nothing = TODO(
-    """
-        Task 4.
-        Rewrite 'JavaCode4.task4()' in Kotlin using lambdas:
-        return true if the collection contains an even number.
-        You can find the appropriate function to call on 'Collection' by using code completion.
-        Don't use the class 'Iterables'.
-    """,
-    documentation = doc4(),
-    references = { JavaCode4().task4(collection) })
+fun todoTask4(collection: Collection<Int>) = collection.any { x -> x % 2 == 0 }
 
 fun task4(collection: Collection<Int>): Boolean = todoTask4(collection)
