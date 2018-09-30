65026b123600d5a8835e752cd3d8969f1087e14b
Raj <the.raj.saxena@gmail.com>
2018-09-30 22:46:02 +0200

    Solution till 41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -1,7 +1,8 @@
 package vi_generics
 
 import util.TODO
-import java.util.*
+import java.util.ArrayList
+import java.util.HashSet
 
 fun task41(): Nothing = TODO(
     """
@@ -13,18 +14,25 @@ fun task41(): Nothing = TODO(
         You should write a function that splits the collection into two collections given as arguments.
         The signature of the 'toCollection()' function from the standard library may help you.
     """,
-        references = { l: List<Int> ->
-            l.partition { it > 0 }
-            l.toCollection(HashSet<Int>())
-        }
+    references = { l: List<Int> ->
+        l.partition { it > 0 }
+        l.toCollection(HashSet<Int>())
+    }
 )
 
+private fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(
+    first: C,
+    second: C,
+    p: (T) -> Boolean
+): Pair<C, C> {
+    val (a, b) = this.partition(p)
+    return a.toCollection(first) to b.toCollection(second)
+}
+
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z' }
 }
